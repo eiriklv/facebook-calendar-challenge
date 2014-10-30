b946678c9f70ec2c9af4b9c0887c7d72da185b15
Eirik Vullum <evullum@gmail.com>
2014-10-31 00:23:51 +0100

    run through jslint

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 var layOutDay = (function() {
+    'use strict';
+
     var selector = 'scheduler';
 
     function renderEvent(start, end, offset, width) {
         return [
             '<div class="event" style="top: ' + start + 'px; left: ' + offset + '%; height: ' + (end - start) + 'px; width: ' + width + '%">',
-                '<div class="content">',
-                    '<div class="inner-content">',
-                        '<span class="event-title">Sample Item</span>',
-                        '<span class="event-location">Sample Location</span>',
-                    '</div>',
-                '</div>',
+            '   <div class="content">',
+            '       <div class="inner-content">',
+            '           <span class="event-title">Sample Item</span>',
+            '           <span class="event-location">Sample Location</span>',
+            '       </div>',
+            '   </div>',
             '</div>'
         ].join('');
     }
 
     function renderLayout(blocks) {
-        var calendarElement = document.getElementById(selector);
-        var width, column, offset;
+        var calendarElement = document.getElementById(selector),
+            width,
+            offset;
 
         calendarElement.innerHTML = '';
 
@@ -31,15 +34,25 @@ var layOutDay = (function() {
                 });
             });
         });
-    };
+    }
+
+    function sortEvents(array, param, desc) {
+        return array.slice().sort(function (a, b) {
+            return desc ? (a[param] + b[param]) : (a[param] - b[param]);
+        });
+    }
 
     function generateLayout(events) {
-        var blocks = [];
-        var sortedEvents = sortEvents(events, 'start');
-        var overlaps, placed, lastBlock;
+        var blocks = [],
+            sortedEvents = sortEvents(events, 'start'),
+            overlaps,
+            placed,
+            lastBlock;
 
         sortedEvents.forEach(function(event) {
-            if (blocks.length === 0) return blocks.push([[event]]);
+            if (blocks.length === 0) {
+                return blocks.push([[event]]);
+            }
 
             placed = false;
             lastBlock = blocks[blocks.length - 1];
@@ -48,7 +61,9 @@ var layOutDay = (function() {
                 return (event.start <= column[column.length - 1].end);
             });
 
-            if (!overlaps) return blocks.push([[event]]);
+            if (!overlaps) {
+                return blocks.push([[event]]);
+            }
 
             lastBlock.forEach(function(column) {
                 if (!placed && (event.start >= column[column.length - 1].end)) {
@@ -56,16 +71,12 @@ var layOutDay = (function() {
                 }
             });
 
-            if (!placed) return lastBlock.push([event])
+            if (!placed) {
+                return lastBlock.push([event]);
+            }
         });
 
         return blocks;
-    }
-
-    function sortEvents(array, param, desc) {
-        return array.slice().sort(function(a, b) {
-            return desc ? (a[param] + b[param]) : (a[param] - b[param]);
-        });
     }
 
     return function(events) {
